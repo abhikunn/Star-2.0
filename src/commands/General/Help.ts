bd641eaa41a1387dfe01397de635a7d4e451992b
abhikunn <93659315+abhikunn@users.noreply.github.com>
2021-11-04 17:36:33 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `✨ *Star command list* ✨\n\n`
+            let text = `❤ *Rem command list*❤ \n\n`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
